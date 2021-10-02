d515c486439a489db74a2abcf463593f0920064d
Mari Taksa <welch.mari@gmail.com>
2021-10-02 15:23:31 -0500

    the modals look better

--- client/src/components/FoodCard.js
+++ client/src/components/FoodCard.js
@@ -17,6 +17,18 @@ function FoodCard({ setfood, food, foods }) {
         const [editing, setEditing] = useState(false)
         const [open, setOpen] = useState(false);
 
+        const style = {
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                width: 400,
+                bgcolor: 'background.paper',
+                border: '2px solid #000',
+                boxShadow: 24,
+                p: 4,
+              };
+
 
         const updateFood = (e) => {
                 e.preventDefault();
@@ -78,12 +90,12 @@ return (
                       aria-labelledby="modal-modal-title"
                       aria-describedby="modal-modal-description"
                     >
-                      <Box>
+                      <Box sx={style}>
                         <Typography id="modal-modal-title" variant="h6" component="h2">
-                          Text in a modal
+                          {food.name}
                         </Typography>
                         <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                          Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                          {food.quantity}
                         </Typography>
                       </Box>
                     </Modal>
