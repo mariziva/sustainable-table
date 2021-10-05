94f08f94618f289ff7ca66673079747e8c3bb071
Mari Taksa <welch.mari@gmail.com>
2021-10-05 09:10:58 -0500

    inserted bg image on homepage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,7 @@
 function Home () {
 
 return(
+<div className="homepage">
 <h1>
     Welcome to Sustainable Table.
     <br />
@@ -8,6 +9,7 @@ return(
     <br />
     Click "My Recipes" to see seven recipes generated with the foods in your fridge. Click each recipe to see details. 
 </h1>
+</div>
 )}
 
 export default Home;
